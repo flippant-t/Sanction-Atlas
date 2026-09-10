17cf1cd16b26b7d1cf0a69208e64e45421adab10
flippant-t <thomasfilippone@aol.com>
2026-09-10 10:47:00 -0400

    Delete functions/api/v1/_middleware.js

--- functions/api/v1/_middleware.js
+++
@@ -1,8 +0,0 @@
-export async function onRequest(context) {
-  if (context.request.method === "OPTIONS") {
-    return new Response(null, { status: 204, headers: { "access-control-allow-origin": "*", "access-control-allow-methods": "GET, POST, OPTIONS", "access-control-allow-headers": "content-type", "access-control-max-age": "86400" } });
-  }
-  const res = await context.next();
-  const h = new Headers(res.headers); h.set("access-control-allow-origin", "*");
-  return new Response(res.body, { status: res.status, headers: h });
-}
